80b44b7a70d4293f0ec43661f05d39ff96eb774c
Innocent Anozie <iygeal@gmail.com>
2024-04-30 00:35:43 +0100

    some changes

--- models/base_model.py
+++ models/base_model.py
@@ -6,21 +6,39 @@ from datetime import datetime
 
 class BaseModel:
     """A base class for all hbnb models"""
+
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
+        """BaseModel constructor method"""
+
+        # Check if kwargs is not empty
+        if kwargs:
+            if "__class__" in kwargs.keys():
+                del kwargs["__class__"]
+                # Convert isoformat strings back to datetime objects
+            if 'created_at' in kwargs.keys():
+                kwargs["created_at"] = datetime.fromisoformat(
+                    kwargs["created_at"])
+            else:
+                self.created_at = datetime.now()
+            if 'updated_at' in kwargs.keys():
+                kwargs["updated_at"] = datetime.fromisoformat(
+                    kwargs["updated_at"])
+            else:
+                self.updated_at = datetime.now()
+            if "id" not in kwargs.keys():
+                self.id = str(uuid.uuid4())
+
+                # Set other keys to their appropriate values
+            for key, value in kwargs.items():
+                if not hasattr(self, key):
+                    setattr(self, key, value)
+        else:
+            # If kwargs is empty, create new attributes
+            import models
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
