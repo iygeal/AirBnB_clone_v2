80e1da03cc21000900670b7802a16db81f3b5b5c
Innocent Anozie <iygeal@gmail.com>
2024-05-01 23:38:39 +0100

    Currently on state.py of task 6

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,21 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import String, Column, DateTime
+
+
+Base = declarative_base()
+"""This is the declaration of Base"""
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    # Class attributes
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.now())
+    updated_at = Column(DateTime, nullable=False, default=datetime.now())
 
     def __init__(self, *args, **kwargs):
         """BaseModel constructor method"""
@@ -29,19 +40,18 @@ class BaseModel:
             if "id" not in kwargs.keys():
                 self.id = str(uuid.uuid4())
 
-                # Set other keys to their appropriate values
+            # Set other keys to their appropriate values
             for key, value in kwargs.items():
-                setattr(self, key, value)
-
-            # Update object with the new parameters
-            #self.__dict__.update(kwargs)
+                # Check if the attribute already exists
+                if not hasattr(self, key):
+                    setattr(self, key, value)
 
         else:
             # If kwargs is empty, create new attributes
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-        models.storage.new(self)
+        # models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -52,14 +62,34 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
+        # Initialize an empty dictionary to hold the converted instance attributes
         dictionary = {}
+
+        # Copy all instance attributes to the dictionary
         dictionary.update(self.__dict__)
+
+        # Check if '_sa_instance_state' key exists in the dictionary
+        if '_sa_instance_state' in dictionary:
+            # If the key exists, remove it from the dictionary
+            del dictionary['_sa_instance_state']
+
+        # Add the '__class__' key to the dictionary with the class name as its value
         dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+                           (str(type(self)).split('.')[-1]).split('\'')[0]})
+
+        # Convert 'created_at' and 'updated_at' attributes to ISO format strings and add them to the dictionary
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+
         return dictionary
+
+    def delete(self):
+        """Delete the current instance from the storage"""
+        # Call the delete method from the storage to remove the instance
+        import models
+        models.storage.delete(self)
