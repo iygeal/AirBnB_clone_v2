82ba5f4c23bb0a9ab1ba762a455d0b26eb8c92db
Innocent Anozie <iygeal@gmail.com>
2024-05-17 00:07:14 +0100

    Refactored task 2 to meet output

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,42 +1,65 @@
-#!/usr/bin/python3
-""" Fabric script """
-from fabric.api import env, put, run, local
-from datetime import datetime
+#!/usr/bin/env python3
+"""
+Fabric script for deploying an archive to web servers
+"""
+
+from fabric.api import env, put, run
 import os
 
+# Server IPs
 env.hosts = ['100.26.216.116', '3.84.161.102']
+env.user = 'ubuntu'  # SSH username
+# Path to SSH private key
+env.key_filename = '~/.ssh/alx_rsa'
 
-
-def do_pack():
-    """ Generates a .tgz file from the contents of 'web_static' folder"""
-    now = datetime.now().strftime('%Y%m%d%H%M%S')
-    archive_path = 'versions/web_static_{}.tgz'.format(now)
-    result = local('tar -czvf {} ./web_static/'.format(archive_path))
-    if result.succeeded:
-        return archive_path
-    else:
-        return None
+# Short variable names for paths
+current_path = '/data/web_static/current'
+releases_path = '/data/web_static/releases'
 
 
 def do_deploy(archive_path):
-    """ distributes an archive to the servers """
+    """
+    Distributes an archive to web servers
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        True if all operations have been done correctly,
+        otherwise returns False.
+    """
     if not os.path.exists(archive_path):
         return False
 
-    file_name = os.path.basename(archive_path).split('.')[0]
-    dest_dir = '/data/web_static/releases/'
-    dest_file = dest_dir + file_name
+    # Extract filename and folder name
+    file_name = os.path.basename(archive_path)
+    folder_name = file_name.replace(".tgz", "")
+    folder_path = f"{releases_path}/{folder_name}/"
 
     try:
-        put(archive_path, '/tmp')
-        run('sudo mkdir -p {}'.format(dest_file))
-        run('sudo tar -xzf /tmp/{}.tgz -C {}'.format(file_name, dest_file))
-        run('sudo rm -f /tmp/{}.tgz'.format(file_name))
-        run('sudo mv {}/web_static/* {}/'.format(dest_file, dest_file))
-        run('sudo rm -rf {}/web_static/*'.format(dest_file))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {} /data/web_static/current'.format(dest_file))
-        print("New version deployed!")
+        # Upload archive to /tmp/ directory
+        put(archive_path, f"/tmp/{file_name}")
+
+        # Create directory for the new version
+        run(f"mkdir -p {folder_path}")
+
+        # Extract archive to the new version directory
+        run(f"tar -xzf /tmp/{file_name} -C {folder_path}")
+
+        # Remove archive from the server
+        run(f"rm -f /tmp/{file_name}")
+
+        # Move contents of web_static folder to new version directory
+        run(f"mv {folder_path}web_static/* {folder_path}")
+
+        # Remove empty web_static folder
+        run(f"rm -rf {folder_path}web_static")
+
+        # Remove current symlink
+        run(f"rm -rf {current_path}")
+
+        # Create new symlink pointing to the new version directory
+        run(f"ln -s {folder_path} {current_path}")
+
+        print('New version deployed!')
         return True
     except Exception as e:
         print("Deployment failed:", e)
