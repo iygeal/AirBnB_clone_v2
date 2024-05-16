485f5d2a55107a37f0a6f4eb454cef6b97cf063b
Innocent Anozie <iygeal@gmail.com>
2024-05-17 00:58:23 +0100

    Refactor

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""Fabric script based on task 3"""
-
-from typing import List
-from fabric.api import lcd, local, cd, run
+"""Fabric script to replicate task 3
+"""
 
 import os
 from fabric.api import *
@@ -11,7 +9,7 @@ from fabric.api import *
 env.hosts = ['100.26.216.116', '3.84.161.102']
 
 
-def do_clean(number: int = 0) -> None:
+def do_clean(number=0):
     """
     Delete out-of-date archives.
 
@@ -23,38 +21,16 @@ def do_clean(number: int = 0) -> None:
     etc.
     """
     # Ensure 'number' is at least 1
-    number = max(1, int(number))
+    number = 1 if int(number) == 0 else int(number)
 
-    # Delete local archives
-    delete_local_archives(number)
-
-    # Delete remote archives
-    delete_remote_archives(number)
-
-
-def delete_local_archives(number: int) -> None:
-    """
-    Delete out-of-date local archives.
-
-    Args:
-        number (int): The number of archives to keep.
-    """
     # List and sort the local archives in the 'versions' directory
     archives = sorted(os.listdir("versions"))
     # Remove the most recent 'number' of archives from the list
-    archives_to_delete = archives[:-number]
+    [archives.pop() for i in range(number)]
     # Delete the remaining archives locally
     with lcd("versions"):
-        [local(f"rm ./{a}") for a in archives_to_delete]
+        [local("rm ./{}".format(a)) for a in archives]
 
-
-def delete_remote_archives(number: int) -> None:
-    """
-    Delete out-of-date remote archives.
-
-    Args:
-        number (int): The number of archives to keep.
-    """
     # Change to the releases directory on the remote servers
     with cd("/data/web_static/releases"):
         # List and sort the remote archives
@@ -62,6 +38,6 @@ def delete_remote_archives(number: int) -> None:
         # Filter out only the web_static archives
         archives = [a for a in archives if "web_static_" in a]
         # Remove the most recent 'number' of archives from the list
-        archives_to_delete = archives[:-number]
+        [archives.pop() for i in range(number)]
         # Delete the remaining archives on the remote servers
-        [run(f"rm -rf ./{a}") for a in archives_to_delete]
+        [run("rm -rf ./{}".format(a)) for a in archives]
