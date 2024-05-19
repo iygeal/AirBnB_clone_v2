862bab5bf4612b221c32f010e0b5822e834151f0
Innocent Anozie <iygeal@gmail.com>
2024-05-19 09:13:58 +0100

    Changes to task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,40 +1,61 @@
 #!/usr/bin/python3
-"""Deploy static to servers"""
+"""
+Fabric script for deploying an archive to web servers
+"""
 
+from fabric.api import *
 import os
-from fabric.api import env, put, run
 
+# Server IPs
 env.hosts = ['100.26.216.116', '3.84.161.102']
-env.user = 'ubuntu'
+env.user = 'ubuntu'  # SSH username
+# Path to SSH private key
 env.key_filename = '~/.ssh/alx_rsa'
+
+# Short variable names for paths
+current_path = '/data/web_static/current'
+releases_path = '/data/web_static/releases'
 
 
 def do_deploy(archive_path):
     """
-    Deploys the static files to the host servers.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        True if all operations have been done correctly,
-        otherwise returns False.
+    Distributes an archive to web servers
     """
     if not os.path.exists(archive_path):
         return False
+
+    # Extract filename and folder name
     file_name = os.path.basename(archive_path)
     folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
+    folder_path = f"{releases_path}/{folder_name}"
+
     try:
-        put(archive_path, "/tmp/{}".format(file_name))
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("rm -rf /tmp/{}".format(file_name))
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
-        run("rm -rf {}web_static".format(folder_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_path))
+        # Upload archive to /tmp/ directory
+        put(archive_path, f"/tmp/{file_name}")
+
+        # Create directory for the new version
+        run(f"mkdir -p {folder_path}")
+
+        # Extract archive to the new version directory
+        run(f"tar -xzf /tmp/{file_name} -C {folder_path}")
+
+        # Remove archive from the server
+        run(f"rm -rf /tmp/{file_name}")
+
+        # Move contents of web_static folder to new version directory
+        run(f"mv {folder_path}/web_static/* {folder_path}/")
+
+        # Remove empty web_static folder
+        run(f"rm -rf {folder_path}/web_static")
+
+        # Remove current symlink
+        run(f"rm -rf {current_path}")
+
+        # Create new symlink pointing to the new version directory
+        run(f"ln -s {folder_path} {current_path}")
+
         print('New version deployed!')
-        success = True
-    except Exception:
-        success = False
-    return success
+        return True
+    except Exception as e:
+        print("Deployment failed:", e)
+        return False
