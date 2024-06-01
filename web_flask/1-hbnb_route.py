2ec9b16a1be630527f8cf1a8d2a4ff9b934594aa
Innocent Anozie <iygeal@gmail.com>
2024-06-01 08:40:51 +0100

    Refactore

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """A python script that starts a Flask web application"""
 
-
 from flask import Flask
 
+
 app = Flask(__name__)
-
-
 @app.route('/', strict_slahses=False)
 def hello_hbnb():
     """"Method that displays greeting"""
