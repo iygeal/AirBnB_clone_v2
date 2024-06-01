b3526af3900d546da30a153329a7672292b480cf
Innocent Anozie <iygeal@gmail.com>
2024-06-01 19:51:39 +0100

    Pycodestyle

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -71,7 +71,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         self.__session = scoped_session(sessionmaker(bind=self.__engine,
                                                      expire_on_commit=False))
-    
+
     def close(self):
         """Close working SQLAlchemy session"""
         self.__session.close()
