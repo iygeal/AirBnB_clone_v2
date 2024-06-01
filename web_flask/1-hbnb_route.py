597abf0cfebf2586c5b6aeb9d89d893897086987
Innocent Anozie <iygeal@gmail.com>
2024-06-01 08:30:12 +0100

    Python Flask script that restarts a web app

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A python script that starts a Flask web application"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slahses=False)
+def hello_hbnb():
+    """"Method that displays greeting"""
+    return "Hello HBNB!"
+
+
+@app.route('hbnb', strict_slashes=False)
+def hbnb():
+    """Method that displays HBNB"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
